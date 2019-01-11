0a2d5d4f53a997ba1e883f6751027fd85cd4b20a
tm <tm@tiejin.cn>
2019-01-11 17:52:51 +0800

    init tfrecords

---
+++ dogVsCat/generateTfrecords.py
@@ -0,0 +1,30 @@
+import os
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# 生成tfrecords文件
+def gen_dogVScat_tfrecords(path):
+    tf_writer = tf.python_io.TFRecordWriter(path)
+    for file in os.listdir("/Users/closer/PycharmProjects/data/dogAndCat2/train/"):
+        if not file.endswith('jpg'):
+            continue
+        label = None
+        if file.startswith("dog"):
+            label = 1
+        else:
+            label = 0
+
+        file_path = "/Users/closer/PycharmProjects/data/dogAndCat2/train/" + file
+        img = Image.open(file_path)
+        img = img.resize((180, 180))
+        img_raw = img.tobytes()
+
+        example = tf.train.Example()
+        feature = example.features.feature
+        feature['img_data'].bytes_list.value.append(img_raw)
+        feature['label'].int64_list.value.append(label)
+        tf_writer.write(example.SerializeToString())
+
+    tf_writer.close()
